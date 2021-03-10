ec0063fb7580103af212ec573e67f14b6f3c1235
Gina Rubira <grubirapa@gmail.com>
2021-03-10 12:50:42 +0100

    Alamofire i primeres dos pantalles creades.

---
+++ bambooNews/Pods/Target Support Files/Pods-bambooNews/Pods-bambooNews-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_bambooNews : NSObject
+@end
+@implementation PodsDummy_Pods_bambooNews
+@end
---
+++ bambooNews/bambooNews/newsViewController.swift
@@ -0,0 +1,15 @@
+//
+//  newsViewController.swift
+//  bambooNews
+//
+//  Created by Georgina Rubira Pairó on 10/03/2021.
+//
+
+import Foundation
+import UIKit
+
+class newsViewController: UITableViewController {
+    override func viewDidLoad() {
+        print("visor noticies creat")
+    }
+}
